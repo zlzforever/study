2ade3db6d3e48f34ec9a349e320f6f83f6ebc7a1
邹嵩 <zousong@research.xbmail.com.cn>
2017-08-17 17:33:23 +0800

    start

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BaiduSearchSpider spider = new BaiduSearchSpider();
+			spider.Run();
+			Console.WriteLine("Compelte.");
+		}
+	}
+}
